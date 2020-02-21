35c0616bd239da055ec58a423ae7002230685661
Nikolai Plath <der.el.kuku@gmail.com>
2020-02-21 18:46:32 -0500

    Init

---
+++ app/src/main/java/org/elkuku/a4emanager/MainActivity.java
@@ -0,0 +1,25 @@
+package org.elkuku.a4emanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mywebview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mywebview = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = mywebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setUserAgentString("4emanager");
+        mywebview.loadUrl("http://agents.enl.ec");
+        mywebview.setWebViewClient(new WebViewClient());
+    }
+}
